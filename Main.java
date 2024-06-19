2f3ecd56440fdd7b2589917df5bf51df19d03cb9
Hayato Suō <midoriya.7140@gmail.com>
2024-06-19 15:33:20 -0300

    wrong numbers entered

--- Main.java
+++ Main.java
@@ -11,17 +11,18 @@ public class Main {
         
         System.out.print("Enter a number between 0 and 10: ");
         int number = sc.nextInt();
-        
+        int i = 0;
         while (number != randomN) {
             if (number < randomN) {
                 System.out.println("Up number");
             } else if (number > randomN) {
                 System.out.println("Down number");
             }
+             i++;
             System.out.print("Wrong number, enter a new number: ");
             number = sc.nextInt();
         }
-        
+        System.out.println("wrong numbers entered "+i);
         System.out.println("Successful! The number was " + randomN);
         sc.close();
     }
